7d31211b9f1b487cbaf3617e8021f3d8d7813786
JCrawshaw16 <joshua.crawshaw@qa.com>
2016-11-22 15:57:20 +0000

    Add files via upload

---
+++ Exercise_1.java
@@ -0,0 +1,12 @@
+
+public class Exercise_1 {
+	public static void main(String[] args){
+		for(int z = 0; z < 10 ; z++){
+			for(int j = 0 ; j < z; j++){
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
